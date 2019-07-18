dbd0bce1e3cebfb513e251add68338480b987502
Artem <savelevart34@gmail.ru>
2019-07-18 21:00:07 +0500

    Part 3. Class-based views, and fantastic generics!

---
+++ snippets/views/__init__.py
@@ -0,0 +1,4 @@
+from .function_based_snippets_views import *
+from .simple_class_based_snippets_views import *
+from .simple_generic_class_bassed_snippets_views import *
+from .mixed_in_generic_views import *
