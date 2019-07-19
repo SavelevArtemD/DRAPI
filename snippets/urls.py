acf344109d6e678a5c45feee6df77478e84fc4fa
Artem <savelevart34@gmail.ru>
2019-07-19 21:17:13 +0500

    Part 4. Work with permissions and set up binding between user and snippet

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets.views.mixed_in_generic_views import SnippetList, SnippetDetail
+from snippets.views.user_views import UserList, UserDetail
 
 urlpatterns = [
+    path('api-auth/', include('rest_framework.urls')),
     path('snippets/', SnippetList.as_view()),
     path('snippets/<int:pk>/', SnippetDetail.as_view()),
+    path('users/', UserList.as_view()),
+    path('users/<int:pk>/', UserDetail.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
